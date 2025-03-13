8f7581f913d5d1ced6c47bd40fc8efa6ea9b6a80
Siddharth Koti <siddharth.koti@gmail.com>
2025-03-13 10:47:36 +0530

    async sequentiall and async parallel

--- resolve promise sequentially.js
+++ resolve promise sequentially.js
@@ -1,51 +1,51 @@
+
+// ------------------
 function createAsyncTasks() {
     return new Promise((resolve, reject) => {
-        
+
         let value = Math.floor(Math.random() * 10);
         console.log(value);
         setTimeout(() => {
-            if(value > 5) {
+            if (value > 5) {
                 resolve('resolved ' + value);
             } else {
                 reject('error ' + value);
             }
-            
+
         }, value * 1000);
     });
 }
 
 const tasks = [
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    ];
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+];
 
-function asyncSequence(tasks, callback) {
-    let error = [];
+async function asyncSequence(tasks, callback) {
+    let errors = [];
     let results = [];
     let completed = 0;
-    
-    tasks.reduce((prev, curr) => {
-        
-        return prev.finally(() => {
-            curr.then((value) => {
-                results.push(value)
-            }, 
-            (err) => {
-                error.push(err)
-            })
-            .finally(() => {
-                completed++;
-                if(completed === tasks.length) {
-                    callback(error, results);
-                }
-            });
-        }); 
-        
-    }, Promise.resolve());
+
+    for (let i = 0; i < tasks.length; i++) {
+        let fn = tasks[i];
+        try {
+            let response = await fn();
+            console.log(response);
+            results.push(response);
+        } catch (err) {
+            console.log(err);
+            errors.push(err);
+        } finally {
+            completed++;
+            if (completed === tasks.length) {
+                callback(errors, results);
+            }
+        }
+    }
 }
 
 // tasks and a callback which is called when all of the promises are resolved
--- resolvePromiseParallely.js
+++ resolvePromiseParallely.js
@@ -1,43 +1,43 @@
 function createAsyncTasks() {
     return new Promise((resolve, reject) => {
-        
+
         let value = Math.floor(Math.random() * 10);
         console.log(value);
         setTimeout(() => {
-            if(value > 5) {
+            if (value > 5) {
                 resolve('resolved ' + value);
             } else {
                 reject('error ' + value);
             }
-            
+
         }, value * 1000);
     });
 }
 
 const tasks = [
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    createAsyncTasks(),
-    ];
-    
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+    createAsyncTasks,
+];
+
 function asyncParallel(tasks, callback) {
     let error = [];
     let results = [];
     let completed = 0;
-    
+
     tasks.forEach((task) => {
-        task.then((value) => {
+        task().then((value) => {
             results.push(value)
         }, (err) => {
             error.push(err)
         }).finally(() => {
             completed++;
-            if(completed === tasks.length) {
-                    callback(error, results);
-                }
+            if (completed === tasks.length) {
+                callback(error, results);
+            }
         });
     });
 }
