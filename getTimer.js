b91a146bc92a8accbea0807ab46b4cf90aa5326d
Siddharth Koti <siddharth.koti@gmail.com>
2025-04-08 10:24:26 +0530

    add these later

---
+++ getTimer.js
@@ -0,0 +1,11 @@
+// given the time in some format, update the remaining time to that day's time in human reable format
+// and it must update the remaing time every second in the timer object  
+
+const timer = {hours: 0, minutes: 0, seconds: 0};
+function getTimer(time, timerObj){
+    
+     
+}
+
+
+console.log(getTimer('2026-06-02T00:00.000-07:00', timer));
